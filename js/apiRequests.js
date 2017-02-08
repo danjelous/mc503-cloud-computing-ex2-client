d5ff9377f203290c0397720b0d5ddbf0bb5e889d
Danjelous <daniel.brunner@live.at>
2017-02-08 15:12:00 +0100

    index adjustments

--- js/apiRequests.js
+++ js/apiRequests.js
@@ -11,20 +11,18 @@ $(document).ready( function(){
             for (var i = 0; i < data.data.length; i++) {
 
                 var currArticle = data.data[i],
-                    currArticleDOM = '<div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src="';
+                    currArticleDOM = '<div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><a href="./productDetail.html?id=' 
+                                     + currArticle.id + '"><img src="';
 
                 // Buildup article DOM
                 // Picture
-                currArticleDOM += hostURL + currArticle.picture_path + '" alt=""><div class="caption"><h4 class="pull-right">';
-
-                // Price
-                currArticleDOM += currArticle.price + '€</h4>';
+                currArticleDOM += hostURL + currArticle.picture_path + '" alt=""><div class="caption">';
 
                 // Name
                 currArticleDOM += '<h4><a href="./productDetail.html?id=' + currArticle.id + '">' + currArticle.name + '</a></h4>';
-                
-                // Description
-                currArticleDOM += '<p>' + currArticle.description + '</p></div></div>';
+
+                // Price
+                currArticleDOM += '<h4>' + currArticle.price + '€</h4></div></div>';
 
                 // Finally add
                 articleRoot.append(currArticleDOM);
