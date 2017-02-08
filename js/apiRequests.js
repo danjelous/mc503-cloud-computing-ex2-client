bcf79b57871797524486bab193fbb47ccc1514b3
Danjelous <daniel.brunner@live.at>
2017-02-08 15:03:25 +0100

    Index site done

--- js/apiRequests.js
+++ js/apiRequests.js
@@ -1,41 +1,41 @@
-var apiURL = "https://hidden-tundra-33627.herokuapp.com/api/";
+var hostURL = "https://hidden-tundra-33627.herokuapp.com/";
+var articleRoot = $(".article-holder");
 
 $(document).ready( function(){
     
     // Index
     if($("body").hasClass("index")) {
-        $.get( apiURL + "articles", function( data ) {
+        $.get( hostURL + "api/articles", function( data ) {
 
             // Add to DOM
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i]);
-            }
+            for (var i = 0; i < data.data.length; i++) {
 
-            //   article-holder
+                var currArticle = data.data[i],
+                    currArticleDOM = '<div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src="';
+
+                // Buildup article DOM
+                // Picture
+                currArticleDOM += hostURL + currArticle.picture_path + '" alt=""><div class="caption"><h4 class="pull-right">';
+
+                // Price
+                currArticleDOM += currArticle.price + '€</h4>';
+
+                // Name
+                currArticleDOM += '<h4><a href="./productDetail.html?id=' + currArticle.id + '">' + currArticle.name + '</a></h4>';
+                
+                // Description
+                currArticleDOM += '<p>' + currArticle.description + '</p></div></div>';
+
+                // Finally add
+                articleRoot.append(currArticleDOM);
+
+                // Check responsive breaks
+                if((i + 1) % 3 == 0) {
+                    articleRoot.append('<div class="clearfix"></div>');
+                }
+
+            }
         });
     }
 
 });
-
-
-    /*        <div class="col-sm-4 col-lg-4 col-md-4">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/320x150" alt="">
-                    <div class="caption">
-                        <h4 class="pull-right">$24.99</h4>
-                        <h4><a href="#">First Product</a>
-                        </h4>
-                        <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                    </div>
-                    <div class="ratings">
-                        <p class="pull-right">15 reviews</p>
-                        <p>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                        </p>
-                    </div>
-                </div>
-            </div>*/
